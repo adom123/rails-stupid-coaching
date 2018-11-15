33a18abe0a7b4b15b6dc001a8c2008e4f05c39a9
Emmanuel Asiama <wagonadom@gmail.com>
2018-11-15 14:47:00 +0200

    stupid-coaching

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,20 @@
+class QuestionsController < ApplicationController
+  def ask
+end
+
+def answer
+@input = params[:question]
+@answer = stupid_coach(@input)
+
+  end
+
+def stupid_coach(question)
+if question == "l am going to  work?" 
+	return "great"
+	elsif question.include? "?"
+     return "Silly question, get dressed and go to work!"
+   else
+     return "I don't care, get dressed and go to work!"
+   end
+  end
+end
